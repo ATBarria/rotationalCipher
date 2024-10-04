a9e015ece99f2c5404d93a333c8a9d888f7c0c54
ATBarria <146737610+ATBarria@users.noreply.github.com>
2024-10-04 18:25:52 +0200

    all task completed

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ Cipher: nopqrstuvwxyzabcdefghijklm
 
 const rotate = (item, number) => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetUpper = alphabet.toUpperCase();
 
   const index = (alphabet.indexOf(item) + number) % 26;
   console.log(item, alphabet[index]);
@@ -21,18 +22,27 @@ rotate("a", 2);
 // with words , working with strings intead of arr
 const rotate2 = (item = "", number) => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetUpper = alphabet.toUpperCase();
   let encryptedWord = "";
 
-  for (const char of item) {
-    console.log(char);
+  for (let i = 0; i < item.length; i++) {
+    console.log(typeof item[i]);
+    if (alphabetUpper.includes(item[i])) {
+      const index = (alphabetUpper.indexOf(item[i]) + number) % 26;
 
-    encryptedWord += alphabet[index];
+      encryptedWord += alphabetUpper[index];
+    } else if (alphabet.includes(item[i])) {
+      const index = (alphabet.indexOf(item[i]) + number) % 26;
+      encryptedWord += alphabet[index];
+    } else {
+      encryptedWord += item[i];
+    }
   }
   console.log(encryptedWord);
   return encryptedWord;
 };
 
-rotate2("CAT", 5);
+rotate2("D O_G", 1);
 
 /*  this work now but not before in exercism because the i wasnt defined!!!!
 
